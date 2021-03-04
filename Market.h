68494f701372785ca26f75c741f3e7e08e7eaaa4
Wei <=>
2021-03-04 16:30:00 -0500

    add Market class and definition of commodities

--- Market.h
+++ Market.h
@@ -2,9 +2,19 @@
 #ifndef STOCKMARKET_MARKET_H
 #define STOCKMARKET_MARKET_H
 
+#include <iostream>
+#include <vector>
+
+#include "Stock.h"
+#include "Commodities.h"
+
+using namespace std;
 
 class Market {
-#END
+private:
+    vector<Stock> stockList;
+    vector<Commodities> commoditiesList;
+public:
 };
 
 
