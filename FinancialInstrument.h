9937c0d6f7bde6bc5158153b5b90fc7312b959b0
Kishan Patel <kishanpat07@gmail.com>
2021-03-04 21:26:12 +0000

    editing fin instr class

---
+++ FinancialInstrument.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+
+class FinancialInstrument {
+  private:
+    string name;
+    double currentPrice;
+    int availableVolume;
+
+  public:
+    void SetName(string passedName);
+    void SetCurrentPrice(double passedCurrentPrice);
+    void SetAvailableVolume(int passedAvailableVolume);
+  
+class FinancialInstrument
+
+
+  + sell
+  + buy
+};
