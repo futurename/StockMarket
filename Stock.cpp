4abe087a8b351ee6d240b55fcd3272e36521cd9b
Wei <=>
2021-03-05 01:17:00 -0500

    add indicator

--- Stock.cpp
+++ Stock.cpp
@@ -1,7 +1,6 @@
 #include "Stock.h"
 #include <iostream>
 #include <iomanip>
-#include <Windows.h>
 
 
 using namespace std;
@@ -35,16 +34,17 @@ string Stock::getTickerSymbol() {
 
 //print stock info
 void Stock::printInfo() const {
-    SetConsoleOutputCP( CP_UTF8 );
+    // SetConsoleOutputCP( CP_UTF8 );
 
-    string indicator;
-    if(previousPrice < 0.001){
-        indicator = " ";
-    }else{
-        indicator =  previousPrice - currentPrice < 0.001 ?"\u2191" : "\u2193";
+    char indicator;
+    if (previousPrice < 0.001) {
+        indicator = ' ';
+    } else {
+        //indicator =  previousPrice - currentPrice < 0.001 ?"\u2191" : "\u2193";
+        indicator = previousPrice - currentPrice < 0.001 ? '+' : '-';
     }
 
-    cout << name << ": [" << setw(2)  << right << uniqueId
+    cout << name << ": [" << setw(2) << right << uniqueId
          << "] | " << setw(10) << companyName
          << " | <" << setw(4) << tickerSymbol
          << "> | " << setw(7) << getIndustryString(industry)
@@ -52,7 +52,9 @@ void Stock::printInfo() const {
          << fixed << setprecision(2) << setw(6) << currentPrice
          << " | pre Price: $" << setw(6) << previousPrice
          << " | ava volume: " << setw(6) << left << availableVolume
-         << " " << indicator <<  endl;
+         << " " << indicator << endl;
+
+    wcout << L'║' << endl;
 }
 
 //setter for volume traded
